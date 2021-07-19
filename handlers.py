b55742cfba25eadf7bb232e868db66d259188be6
MrKajibmap <nik.borz@mail.ru>
2021-07-19 12:52:31 +0300

    Full system resources management functionality added

--- handlers.py
+++ handlers.py
@@ -1,6 +1,7 @@
 from main import bot, dp, connection
 from config import ADMIN_ID, button_request_errors_text, button_request_status_text, \
-    button_request_rtp_text, button_request_vf_text, DATABASELINK_POSTGRES, button_show_errors
+    button_request_rtp_text, button_request_vf_text, DATABASELINK_POSTGRES, button_show_errors, \
+    button_show_resources
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove, ReplyKeyboardRemove
 from aiogram.dispatcher.filters import Text
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
@@ -9,6 +10,7 @@ from typing import List
 reply_markup = ReplyKeyboardMarkup(
     keyboard=[
         [
+            KeyboardButton(text=button_show_resources),
             KeyboardButton(text=button_request_errors_text),
             KeyboardButton(text=button_request_status_text),
             KeyboardButton(text=button_request_rtp_text),
@@ -78,6 +80,7 @@ async def show_errors(message: Message):
         				""")
 
     query_results = cursor.fetchall()
+    cursor.close()
     text = '\n\n'.join([', '.join(map(str, x)) for x in query_results])
     if len(text) > 0:
         await bot.send_message(
@@ -119,6 +122,7 @@ async def show_errors(message: Message):
        			order by start_dttm
            				""")
     query_results = cursor.fetchall()
+    cursor.close()
     if len(query_results) > 0:
         text = '\n\n'.join([', '.join(map(str, x)) for x in query_results])
     elif len(query_results) == 0:
@@ -156,6 +160,7 @@ async def show_errors(message: Message):
         				limit 30
     	) t1 order by start_dttm """)
     query_results = cursor.fetchall()
+    cursor.close()
     text = '\n\n'.join([', '.join(map(str, x)) for x in query_results])
     if len(text) > 0:
         await bot.send_message(
@@ -197,6 +202,7 @@ select cast(lower(process_nm) as varchar(32)) as process_nm,
     				limit 30
 	) t1 order by start_dttm """)
     query_results = cursor.fetchall()
+    cursor.close()
     text = '\n\n'.join([', '.join(map(str, x)) for x in query_results])
     if len(text) > 0:
         await bot.send_message(
@@ -216,6 +222,7 @@ def get_errors_list_nms() -> List[str]:
                    "from etl_cfg.cfg_status_table\n"
                    "where status_cd='E'\n")
     query_results = cursor.fetchall()
+    cursor.close()
     if len(query_results) > 0:
         list_results = ', '.join([', '.join(map(str, x)) for x in query_results]).split((', '))
     elif len(query_results) == 0:
@@ -224,12 +231,25 @@ def get_errors_list_nms() -> List[str]:
 
 
 async def errors_keyboard():
-    markup = InlineKeyboardMarkup()
+    markup = InlineKeyboardMarkup(row_width=2)
     err_processes = get_errors_list_nms()
     if len(get_errors_list_nms()) > 0:
         for resource in err_processes:
             markup.add(InlineKeyboardButton(resource, callback_data=resource))
-            return markup
+        return markup
+
+
+async def get_keyboard(list, type_nm=''):
+    markup = InlineKeyboardMarkup(row_width=2)
+    print(f'type_nm = {type_nm}')
+    if len(list) > 0:
+        for obj in list:
+            print(obj)
+            if len(type_nm) > 0:
+                markup.add(InlineKeyboardButton(obj, callback_data=f"{obj}+{type_nm}"))
+            elif len(type_nm) == 0:
+                markup.add(InlineKeyboardButton(obj, callback_data=obj))
+        return markup
 
 
 def close_resource_status(resource_nm):
@@ -237,6 +257,7 @@ def close_resource_status(resource_nm):
     cursor.execute(
         f"update etl_cfg.cfg_status_table set status_cd='C' where status_cd='E' and resource_nm = '{resource_nm}'")
     connection.commit()
+    cursor.close()
 
 
 def update_resource_status(resource_nm):
@@ -244,6 +265,151 @@ def update_resource_status(resource_nm):
     cursor.execute(
         f"update etl_cfg.cfg_status_table set status_cd='A' where status_cd='E' and resource_nm = '{resource_nm}'")
     connection.commit()
+    cursor.close()
+
+
+def open_resource_status(resource_nm):
+    cursor = connection.cursor()
+    print('resource_nm=====', resource_nm)
+    cursor.execute(
+        f"select 1012 as ex_flg from etl_cfg.cfg_status_table where lower(resource_nm) = '{resource_nm}'")
+    # Если ресурс УЖЕ имеется в статусной таблице за сегодня (значит, зависимые ресурсы также имеются в статусе "L"
+    # if int(cursor.fetchone()[0]) == 1:
+    query_rc = cursor.fetchall()
+    query_list = ', '.join([', '.join(map(str, x)) for x in query_rc]).split((', '))
+    print('QUERY=======', query_list)
+    print('type of result=', type(query_list))
+    print('length of list =====', len(query_list))
+    if len(query_list) >= 4:
+        if int(query_list[0]) == 1:
+            print('Ветка, если полученный ответ не типа НонТайп')
+            # Если ресурс уже есть в системе, то надо выкинуть предупреждение и другую клавиатуру - перезапускать ли
+            # в этом случае. Если да: Удаляем текущий ресурс из таблицы статусов
+            cursor.execute(f"delete from etl_cfg.cfg_status_table where lower(resource_nm) = '{resource_nm}'")
+            # находим зависимые ресурсы, статус которых нужно проставить в "А", чтобы основной ресурс запустился
+            cursor.execute(
+                f"select replace(rule_cond, '/A','') from etl_cfg.cfg_schedule_rule where lower(rule_nm) = '{resource_nm}'")
+            query_results = cursor.fetchall()
+            if len(query_results) > 0:
+                print('Ветка, если полученный ответ для определения зависимых ресурсов не нулевой')
+                list_results = ', '.join([', '.join(map(str, x)) for x in query_results]).split((', '))
+                print('зависимые ресурсы: ', list_results)
+                i = 1
+                for obj in list_results:
+                    i = i + 1
+                    print(f'зависимый ресурс #{i}: ', obj)
+                    # Обновляем зависимый ресурс в "А" для запуска главного ресурса
+                    cursor.execute(f"update etl_cfg.cfg_status_table set status_cd = 'A' where resource_nm = '{obj}'")
+                    print(f"Статус ресурса {obj} обновлен в 'A'.")
+            else:
+                print(f'Для указанного ресурса {resource_nm} нет правила запуска! Обратитесь к администратору')
+            connection.commit()
+        else:
+            print(
+                f'Боже как ты вообще попал в эту ветку - ответ есть, а при этом ресурс {resource_nm} в таблице статусов не найден')
+    # Если ресурса НЕТ в статусной таблице за сегодня (но, возможно, он загружается - надо проверить)
+    elif len(query_list) <= 1:
+        # находим зависимые ресурсы, статус которых нужно проставить в "А", чтобы основной ресурс запустился
+        cursor.execute(
+            f"select replace(rule_cond, '/A','') from etl_cfg.cfg_schedule_rule where lower(rule_nm) = '{resource_nm}'")
+        query_results = cursor.fetchall()
+        print(query_results)
+        if len(query_results) > 0:
+            print('Ветка, если есть зависимые ресурсы')
+            list_results = ', '.join([', '.join(map(str, x)) for x in query_results]).split((' '))
+            print('list_results ===== ', list_results)
+            i = 0
+            for obj in list_results:
+                i = i + 1
+                print(f'зависимый ресурс #{i}: ', obj)
+                # Проверка на существование ресурса в таблице статусов за сегодня
+                cursor.execute(f"select 1012 as ex_flg from etl_cfg.cfg_status_table where resource_nm = '{obj}'")
+                query_rc = cursor.fetchall()
+                query_list_inter = ', '.join([', '.join(map(str, x)) for x in query_rc]).split((', '))
+                # print('type=',type(query_list_inter))
+                # print('Value',query_list_inter)
+                if len(query_list_inter) >= 4:
+                    # Если ресурс УЖЕ имеется в статусной таблице за сегодня (значит, зависимые ресурсы также имеются в статусе "L"
+                    if int(query_list_inter[0]) == 1:
+                        print(
+                            f"Зависимый ресурс {obj} уже имеется в таблице статусов за сегодня. Ожидайте регламентного запуска процесса (каждые 15 мин).")
+                        cursor.execute(
+                            f"update etl_cfg.cfg_status_table set status_cd = 'A' where resource_nm = '{obj}' and status_cd not in ('P', 'E')")
+                        print(
+                            f"Статус зависимого ресурса {obj} обновлен в 'A', на случай, если его текущий статус был закрыт вручную.")
+                elif len(query_list_inter) <= 1:
+                    cursor.execute(f"select resource_id from  etl_cfg.cfg_resource where lower(resource_nm) = '{obj}'")
+                    resource_id = cursor.fetchall()
+                    resource_id_fmt = ', '.join([', '.join(map(str, x)) for x in resource_id]).split((', '))
+                    # print('======================',resource_id_fmt)
+                    # print('type of first object in a list ===', type(int(resource_id_fmt[0].split('.')[0])))
+                    # print('res_id====',int(resource_id_fmt[0].split('.')[0]))
+                    postgres_insert_query = """ INSERT INTO etl_cfg.cfg_status_table(resource_id, resource_nm, status_cd, processed_dttm, retries_cnt) 	VALUES (%s,%s,%s,%s,%s)"""
+                    record_to_insert = (int(resource_id_fmt[0].split('.')[0]), obj, 'A', 'now()', 0)
+                    cursor.execute(postgres_insert_query, record_to_insert)
+                    print(f"Добавлена новая строка - статус ресурс {obj} = 'A'")
+                else:
+                    pass
+        connection.commit()
+    else:
+        print('ЕЕЕ ПАРНИ ХЗ ЧТО ПРОИСХОДИТ ЕЕЕЕЕЕ')
+    cursor.close()
+
+
+def show_resource_status(resource_nm):
+    cursor = connection.cursor()
+    # проверка на существование статуса для указанного ресурса
+    cursor.execute(f"select 1012 as ex_flg from etl_cfg.cfg_status_table where lower(resource_nm) = '{resource_nm}'")
+    query_rc = cursor.fetchall()
+    query_list = ', '.join([', '.join(map(str, x)) for x in query_rc]).split((', '))
+    if len(query_list) >= 4:
+        cursor.execute(f"select status_cd from etl_cfg.cfg_status_table where lower(resource_nm) = '{resource_nm}'")
+        query_rc = cursor.fetchall()
+        query_list = ', '.join([', '.join(map(str, x)) for x in query_rc]).split((', '))
+        return query_list[0]
+    else:
+        return 'Ресурс отсутствует в таблице статусов'
+
+
+def get_modules_list() -> List[str]:
+    cursor = connection.cursor()
+    cursor.execute("select distinct lower(module_nm) from etl_cfg.cfg_resource")
+    query_results = cursor.fetchall()
+    cursor.close()
+    if len(query_results) > 0:
+        list_results = ', '.join([', '.join(map(str, x)) for x in query_results]).split((', '))
+    elif len(query_results) == 0:
+        list_results = ''
+    return list_results
+
+
+def get_resources_list(module_nm) -> List[str]:
+    cursor = connection.cursor()
+    cursor.execute(
+        f"select distinct lower(resource_nm) from etl_cfg.cfg_resource where lower(module_nm) = '{module_nm}' and resource_nm not like '%_rtp'")
+    query_results = cursor.fetchall()
+    cursor.close()
+    if len(query_results) > 0:
+        list_results = ', '.join([', '.join(map(str, x)) for x in query_results]).split((', '))
+    elif len(query_results) == 0:
+        list_results = ''
+    return list_results
+
+
+@dp.message_handler(Text(equals=button_show_resources))
+async def show_modules(message: Message):
+    if len(get_modules_list()) > 0:
+        markup = InlineKeyboardMarkup()
+        await bot.send_message(
+            chat_id=message.from_user.id,
+            text='<b>Выберите модуль: </b>\n',
+            reply_markup=await get_keyboard(get_modules_list())
+        )
+    elif len(get_modules_list()) == 0:
+        await bot.send_message(
+            chat_id=message.from_user.id,
+            text='Модули отсутствуют.\n\n<b>Сообщите администратору!</b>'
+        )
 
 
 @dp.message_handler(Text(equals=button_show_errors))
@@ -265,6 +431,7 @@ async def show_errors(message: Message):
 @dp.callback_query_handler(lambda call: True)
 async def callback_inline(call):
     call_data = call.data
+    print(call_data)
     if call.data in get_errors_list_nms():
         markup = InlineKeyboardMarkup()
         markup.add(InlineKeyboardButton('Перезапустить', callback_data=f'{call.data}+res_act_rerun'))
@@ -277,7 +444,17 @@ async def callback_inline(call):
             text=f'Как обработать статус ресурса {call_data}?\n',
             reply_markup=markup
         )
-    elif call_data.find('+'):
+    elif call.data in get_modules_list():
+        print(f'privet {call.data}')
+        await bot.edit_message_text(
+            chat_id=call.from_user.id,
+            message_id=call.message.message_id,
+            text=f"<b>Выберите ресурсы, доступные для модуля {call.data}: </b>\n",
+            reply_markup=await get_keyboard(get_resources_list(call.data), type_nm='single_resource')
+        )
+    elif call_data.find('+') != -1:
+        print(f'CALLBACK={call_data}')
+        print(f"Результат поиска плюсика = {call_data.find('+')}")
         if call_data.split('+')[1] == 'res_act_rerun':
             update_resource_status(call_data.split('+')[0])
             if len(get_errors_list_nms()) > 0:
@@ -313,7 +490,8 @@ async def callback_inline(call):
                 await bot.edit_message_text(
                     chat_id=call.from_user.id,
                     message_id=call.message.message_id,
-                    text=f"<b>Статус процесса {call_data.split('+')[0]} был обновлен с 'E' на 'С'.</b>\n\n<b>Отсутствуют "
+                    text=f"<b>Статус процесса {call_data.split('+')[0]} был обновлен с 'E' на "
+                         f"'С'.</b>\n\n<b>Отсутствуют "
                          f"процессы, завершившиеся с ошибками.</b>\n",
                     reply_markup=await errors_keyboard()
                 )
@@ -335,3 +513,70 @@ async def callback_inline(call):
                          f"процессы, завершившиеся с ошибками.</b>\n",
                     reply_markup=await errors_keyboard()
                 )
+        # Блок обработчика для управления отдельными процессами
+        elif call_data.split('+')[1] == 'single_resource':
+            markup = InlineKeyboardMarkup(row_width=2)
+            markup.add(InlineKeyboardButton('Запустить', callback_data=f"{call_data.split('+')[0]}+single_res_act_run"),
+                       (
+                           InlineKeyboardButton('Узнать статус',
+                                                callback_data=f"{call_data.split('+')[0]}+single_res_act_status")),
+                       InlineKeyboardButton('Закрыть', callback_data=f"{call_data.split('+')[0]}+single_res_act_close"),
+                       InlineKeyboardButton('Пропустить',
+                                            callback_data=f"{call_data.split('+')[0]}+single_res_act_skip"))
+            # заряжаем новую
+            await bot.edit_message_text(
+                chat_id=call.from_user.id,
+                message_id=call.message.message_id,
+                text=f"Как обработать ресурс {call_data.split('+')[0]}?\n",
+                reply_markup=markup
+            )
+        elif call_data.split('+')[1] == 'single_res_act_run':
+            open_resource_status(call_data.split('+')[0])
+            await bot.edit_message_text(
+                chat_id=call.from_user.id,
+                message_id=call.message.message_id,
+                text=f"<b>Ресурс {call_data.split('+')[0]} запустится при следующей регламетной проверке статусов (каждые 15 минут).</b>\n\n"
+                     f"Как поступим с другими ресурсами? \n"
+                     f"<b>Выберите модуль: </b> \n",
+                reply_markup=await get_keyboard(get_modules_list())
+            )
+        elif call_data.split('+')[1] == 'single_res_act_status':
+            await bot.edit_message_text(
+                chat_id=call.from_user.id,
+                message_id=call.message.message_id,
+                text=f"<b>Текущий статус ресурса {call_data.split('+')[0]}: '{show_resource_status(call_data.split('+')[0])}'.</b>\n\n"
+                     f"Как поступим с другими ресурсами? \n"
+                     f"<b>Выберите модуль: </b>\n",
+                reply_markup=await get_keyboard(get_modules_list())
+            )
+        elif call_data.split('+')[1] == 'single_res_act_close':
+            open_resource_status(call_data.split('+')[0])
+            await bot.edit_message_text(
+                chat_id=call.from_user.id,
+                message_id=call.message.message_id,
+                text=f"<b>Текущий статус ресурса {call_data.split('+')[0]}: '{show_resource_status(call_data.split('+')[0])}'.</b>\n\n"
+                     f"Как поступим с другими ресурсами? \n"
+                     f"<b>Выберите модуль: </b>\n",
+                reply_markup=await get_keyboard(get_modules_list())
+            )
+        elif call_data.split('+')[1] == 'single_res_act_skip':
+            await bot.edit_message_text(
+                chat_id=call.from_user.id,
+                message_id=call.message.message_id,
+                text=f"<b>Пропуск хода.</b>\n\n"
+                     f"Как поступим с другими ресурсами? \n"
+                     f"<b>Выберите модуль: </b>\n",
+                reply_markup=await get_keyboard(get_modules_list())
+            )
+        else:
+            print(f'УУУУУХ Я ХЗ ЧЕ ЗА КОМАНДА ПАЦАНЫ!!!!!!!')
+            await bot.send_message(
+                chat_id=call.from_user.id,
+                text=f'УУУУУХ Я ХЗ ЧЕ ЗА КОМАНДА ПАЦАНЫ!!!!!!! callback={call_data}\n'
+            )
+    else:
+        print(f'УУУУУХ Я ХЗ ЧЕ ЗА КОМАНДА ПАЦАНЫ!!!!!!!')
+        await bot.send_message(
+            chat_id=call.from_user.id,
+            text='УУУУУХ Я ХЗ ЧЕ ЗА КОМАНДА ПАЦАНЫ!!!!!!! callback={call_data}\n'
+        )
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from aiogram import Bot, Dispatcher, executor
 import asyncio
+
+from aiogram.types import InlineKeyboardButton
+
 from config import TG_TOKEN, DATABASELINK_POSTGRES
 import dj_database_url
 import psycopg2
@@ -20,3 +23,25 @@ if __name__ == '__main__':
     #
     # executor.start_polling(dispatcher=dp, on_startup=send_to_admin)
     import time
+    def get_modules_list(l: list) -> list:
+        for obj in l:
+            print(obj)
+        return l
+    get_modules_list([1,2])
+    pr='1_23'
+    print(pr)
+    if pr.find('+')!=-1:
+        print(f"result={pr.find('+')}")
+    elif call_data.split('+')[1] == 'single_resource':
+        markup = InlineKeyboardMarkup(row_width=2)
+        markup.add(InlineKeyboardButton('Запустить', callback_data=f"{call_data.split('+')[0]}+single_res_act_rerun"), (
+            InlineKeyboardButton('Узнать статус', callback_data=f"{call_data.split('+')[0]}+single_res_act_status")),
+            InlineKeyboardButton('Закрыть', callback_data=f"{call_data.split('+')[0]}+single_res_act_close"),
+            InlineKeyboardButton('Пропустить', callback_data=f"{call_data.split('+')[0]}+single_res_act_skip"))
+        # заряжаем новую
+        await bot.edit_message_text(
+            chat_id=call.from_user.id,
+            message_id=call.message.message_id,
+            text=f"Как обработать ресурс {call_data.split('+')[0]}?\n",
+            reply_markup=markup
+        )
